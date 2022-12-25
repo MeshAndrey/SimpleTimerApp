089a9dc0498a83ac096d2164f9c0bdacef1ef34a
MeshAndrey <meshcheryakov12a@gmail.com>
2022-12-25 17:53:17 +0300

    Add basic gui code

--- src/widgets/InputWidget.cpp
+++ src/widgets/InputWidget.cpp
@@ -2,7 +2,30 @@
 
 InputWidget::InputWidget(QWidget *parent) : QWidget(parent)
 {
-    QVBoxLayout *layout = new QVBoxLayout ();
-    layout->addWidget(new QLabel("Placeholder"));
-    setLayout(layout);
+    QVBoxLayout *verLayout = new QVBoxLayout;
+    QHBoxLayout *topHorLayout = new QHBoxLayout;
+    QGridLayout *gridLayout = new QGridLayout;
+
+    int buttonCounter = 0;
+    for (int row = 0; row < 3 ; row++)
+    {
+        for (int column = 0; column < 3; column++)
+        {
+            QPushButton * btn = new QPushButton(QString::number(++buttonCounter));
+            gridLayout->addWidget(btn, row, column);
+        }
+    }
+    gridLayout->addWidget(new QPushButton("0"), 3, 1);
+
+    topHorLayout->addWidget(new QLineEdit);
+    topHorLayout->addWidget(new QLabel("H"));
+    topHorLayout->addWidget(new QLineEdit);
+    topHorLayout->addWidget(new QLabel("Min"));
+    topHorLayout->addWidget(new QLineEdit);
+    topHorLayout->addWidget(new QLabel("Sec"));
+
+    verLayout->addLayout(topHorLayout);
+    verLayout->addLayout(gridLayout);
+
+    setLayout(verLayout);
 }
