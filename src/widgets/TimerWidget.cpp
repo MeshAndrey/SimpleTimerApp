eb22a7c2e175ddb249e7da0407df5c3dea490b7c
MeshAndrey <meshcheryakov12a@gmail.com>
2023-01-07 14:07:25 +0300

    Add base timer widget code

--- src/widgets/TimerWidget.cpp
+++ src/widgets/TimerWidget.cpp
@@ -2,5 +2,18 @@
 
 TimerWidget::TimerWidget(QWidget *parent) : QWidget(parent)
 {
+    QVBoxLayout * layout = new QVBoxLayout;
+    QHBoxLayout * horLayout = new QHBoxLayout;
 
+    pauseResumeButton = new QPushButton("Pause");
+    stopButton = new QPushButton("Stop");
+    remainingTimeLabel = new QLabel("Text");
+
+    horLayout->addWidget(pauseResumeButton);
+    horLayout->addWidget(stopButton);
+
+    layout->addWidget(remainingTimeLabel);
+    layout->addLayout(horLayout);
+
+    setLayout(layout);
 }
--- src/widgets/TimerWidget.h
+++ src/widgets/TimerWidget.h
@@ -3,10 +3,19 @@
 
 #include <QObject>
 #include <QWidget>
+#include <QtWidgets>
 
 class TimerWidget : public QWidget
 {
     Q_OBJECT
+private:
+    QLabel *remainingTimeLabel = nullptr;
+    QPushButton *pauseResumeButton = nullptr,
+                *stopButton        = nullptr;
+
+    QTimer *timer = nullptr,
+           *updateTimer = nullptr;
+
 public:
     explicit TimerWidget(QWidget *parent = nullptr);
 
